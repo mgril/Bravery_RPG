6d1569ac05749374fe963e38459ecca4d3318c74
mgril <112563882+mgril@users.noreply.github.com>
2024-02-13 17:22:30 +0100

    assets player scripts

---
+++ Assets/Player/PlayerWinState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWinState : PlayerState
+{
+    public PlayerWinState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void AnimationFinishTrigger()
+    {
+        base.AnimationFinishTrigger();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        GameObject.Find("Canvas").GetComponent<UI>().SwitchOnNextLevelScreen();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        player.SetZeroVelocity();
+    }
+}
